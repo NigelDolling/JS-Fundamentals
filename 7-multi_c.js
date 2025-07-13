d0bb10229fd9ff3df0f8baf0cfb3d265ad2f2435
Nigel Dolling <61507887+NigelDolling@users.noreply.github.com>
2025-07-13 23:23:33 +0000

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,7 @@ const arg = parseInt(process.argv[2]);
 
 if (isNaN(arg)) {
     console.log('Missing number of occurrences');
-} else {
+} else if (arg > 0 ) {
     let output = '';
     for (let i = 0; i < arg; i++) {
         output += 'C is fun\n';
